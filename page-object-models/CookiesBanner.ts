f53cd0f2f186700c3e5ddd15aead00c08ff9c600
amela vorgic <amela.vorgic@partner.bmwgroup.com>
2025-03-24 14:32:32 +0100

    Formated tests

--- page-object-models/CookiesBanner.ts
+++ page-object-models/CookiesBanner.ts
@@ -1,30 +1,26 @@
 import { expect, type Locator, type Page } from '@playwright/test';
 
 export class CookiesBanner {
-  private page: Page;
   cookieBanner: Locator;
   agreeButton: Locator;
   disagreeButton: Locator;
-  
+
   constructor(page: Page) {
-    this.page = page;
     this.cookieBanner = page.locator('#qc-cmp2-ui');
     this.agreeButton = page.getByRole('button', { name: 'AGREE', exact: true });
     this.disagreeButton = page.getByRole('button', { name: 'DISAGREE' });
-}
-
-async acceptCookies() {
-  if (await this.cookieBanner.isVisible()) {
-    await this.agreeButton.click();
   }
-}
 
-
-async rejectCookies() {
-  if (await this.cookieBanner.isVisible()) {
-    await this.disagreeButton.click();
+  async acceptCookies() {
+    if (await this.cookieBanner.isVisible()) {
+      await this.agreeButton.click();
+    }
   }
-}
 
-  
+  async rejectCookies() {
+    if (await this.cookieBanner.isVisible()) {
+      await this.disagreeButton.click();
+    }
+  }
+
 }
--- page-object-models/CreateNewAccountForm.ts
+++ page-object-models/CreateNewAccountForm.ts
@@ -1,41 +1,33 @@
 import { expect, type Locator, type Page } from '@playwright/test';
 
 export class CreateNewAccountForm {
-  private page: Page;
   firstNameInput: Locator;
   lastNameInput: Locator;
   emailInput: Locator;
   passwordInput: Locator;
   confirmPasswordInput: Locator;
   createAnAccountButton: Locator;
-
   firstNameError: Locator;
   lastNameError: Locator;
   emailError: Locator;
   passwordError: Locator;
   confirmPasswordError: Locator;
-
   errorMessages: Locator;
   passwordStrengthMeter: Locator;
   errorBanner: Locator;
 
   constructor(page: Page) {
-    this.page = page;
     this.firstNameInput = page.locator('#firstname');
     this.lastNameInput = page.locator('#lastname');
     this.emailInput = page.locator('#email_address');
     this.passwordInput = page.locator('#password');
     this.confirmPasswordInput = page.locator('#password-confirmation');
     this.createAnAccountButton = page.locator('button[title="Create an Account"]');
-
-
     this.firstNameError = page.locator('#firstname-error');
     this.lastNameError = page.locator('#lastname-error');
     this.emailError = page.locator('#email_address-error');
     this.passwordError = page.locator('#password-error');
     this.confirmPasswordError = page.locator('#password-confirmation-error');
-
-
     this.errorMessages = page.locator('.mage-error');
     this.passwordStrengthMeter = page.locator('.password-strength-meter');
     this.errorBanner = page.locator('.message-error');
@@ -51,6 +43,7 @@ export class CreateNewAccountForm {
 
   async shouldRequireAllFields() {
     const errorMessages = await this.errorMessages.allTextContents();
+    console.log(errorMessages);
     await expect(errorMessages).toContain('This is a required field.');
   }
 
@@ -58,11 +51,11 @@ export class CreateNewAccountForm {
     await this.emailInput.fill(email);
   }
 
-  async shouldShowEmailFormatError(){
+  async shouldShowEmailFormatError() {
     await expect(this.emailError).toHaveText('Please enter a valid email address (Ex: johndoe@domain.com).');
   }
 
-  async shouldShowEmailAlreadyRegisteredError(){
+  async shouldShowEmailAlreadyRegisteredError() {
     await expect(this.errorBanner).toHaveText('There is already an account with this email address. If you are sure that it is your email address, click here to get your password and access your account.');
   }
 
@@ -70,38 +63,37 @@ export class CreateNewAccountForm {
     await this.passwordInput.fill(password);
   }
 
-  async shouldShowWeakPasswordMessage(){
+  async shouldShowWeakPasswordMessage() {
     await expect(this.passwordStrengthMeter).toBeVisible();
     await expect(this.passwordStrengthMeter).toContainText('Weak');
     await expect(this.passwordError).toHaveText('Minimum of different classes of characters in password is 3. Classes of characters: Lower Case, Upper Case, Digits, Special Characters.');
   }
 
-  async shouldShowPasswordLengthError(){
+  async shouldShowPasswordLengthError() {
     await expect(this.passwordStrengthMeter).toBeVisible();
     await expect(this.passwordStrengthMeter).toContainText('Weak');
     await expect(this.passwordError).toHaveText('Minimum length of this field must be equal or greater than 8 symbols. Leading and trailing spaces will be ignored.');
   }
 
-  async shouldIndicateStrongPassword(){
+  async shouldIndicateStrongPassword() {
     await expect(this.passwordStrengthMeter).toBeVisible();
-    await expect(this.passwordStrengthMeter).toContainText('Strong');  
+    await expect(this.passwordStrengthMeter).toContainText('Strong');
   }
 
   async fillConfirmPassword(confirmPassword: string) {
     await this.confirmPasswordInput.fill(confirmPassword);
   }
 
-  async shouldShowPasswordMismatchError(){
+  async shouldShowPasswordMismatchError() {
     await expect(this.confirmPasswordError).toHaveText('Please enter the same value again.');
   }
 
-  async shouldNavigateToCustomerAccountPage(page){
+  async shouldNavigateToCustomerAccountPage(page: Page) {
     await expect(page).toHaveURL(/.*customer\/account/);
   }
 
   async submitForm() {
-    await this.
-    createAnAccountButton.click();
+    await this.createAnAccountButton.click();
   }
 
   async clearForm() {
@@ -110,6 +102,6 @@ export class CreateNewAccountForm {
     await this.emailInput.clear();
     await this.passwordInput.clear();
     await this.confirmPasswordInput.clear();
-  }  
-  
+  }
+
 }
