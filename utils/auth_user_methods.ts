30b4db66fd127309b38227711d0afd23f5824271
amela vorgic <amela.vorgic@partner.bmwgroup.com>
2025-04-08 15:31:37 +0200

    refactor auth methods

--- utils/auth_user_methods.ts
+++ utils/auth_user_methods.ts
@@ -1,9 +1,14 @@
 import { Page } from '@playwright/test';
 import { CreateNewAccountForm } from '../page-object-models/CreateNewAccountForm';
+import { LoginPage } from '../page-object-models/LoginPage';
 import { CookiesBanner } from '../page-object-models/CookiesBanner';
 import { faker } from '@faker-js/faker';
+import * as fs from 'fs';
 
 export let password: string;
+
+const COOKIE_FILE_PATH = 'cookies.json';
+
 export async function registerNewTestUser(page: Page) {
     await page.goto('/customer/account/create/');
     const cookiesBanner = new CookiesBanner(page);
@@ -26,5 +31,22 @@ export async function registerNewTestUser(page: Page) {
         registeredUser.password
     );
 
-    return registeredUser; 
+    return registeredUser;
+
 }
+
+export async function loginTestUser(page: Page, registeredUser: { email: string; password: string }) {
+    await page.goto('/customer/account/login/');
+    const cookiesBanner = new CookiesBanner(page);
+    await page.waitForLoadState('networkidle');
+    await cookiesBanner.acceptCookies();
+
+    const loginPage = new LoginPage(page);
+    await loginPage.login(registeredUser.email, registeredUser.password);
+
+    const cookies = await page.context().cookies();
+    fs.writeFileSync(COOKIE_FILE_PATH, JSON.stringify(cookies, null, 2));
+}
+
+
+
