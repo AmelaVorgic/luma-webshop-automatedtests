5a989746a052c6ff59679c19ed7cc3d694ec7a35
amela vorgic <amela.vorgic@partner.bmwgroup.com>
2025-04-18 10:14:58 +0200

    new auth setup

---
+++ tests/auth.spec.ts
@@ -0,0 +1,25 @@
+import { test as setup } from '@playwright/test';
+import dotenv from 'dotenv';
+import { LoginPage } from '../page-object-models/LoginPage';
+import { CookiesBanner } from '../page-object-models/CookiesBanner';
+import path from 'path';
+
+dotenv.config();
+
+const authFile = path.join(__dirname, '../playwright/.auth/user.json');
+let cookiesBanner: CookiesBanner;
+
+setup('authenticate', async ({ page }) => {
+    let loginPage: LoginPage;
+
+    const username = process.env.VALID_USERNAME as string;
+    const password = process.env.VALID_PASSWORD as string;
+
+    await page.goto('/customer/account/login/');
+    cookiesBanner = new CookiesBanner(page);
+    await page.waitForLoadState('networkidle');
+    await cookiesBanner.acceptCookies(); 
+    loginPage = new LoginPage(page);    await loginPage.login(username, password);
+
+    await page.context().storageState({ path: authFile });
+});
